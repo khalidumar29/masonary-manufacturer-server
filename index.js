bfd61402e62585dbed4f809c4aa53ed690765aa6
Khalid Umar <63781725+khalidumar29@users.noreply.github.com>
2022-05-28 02:05:44 +0600

    done projects

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 
-/** middle ware */
+/** middle wares */
 app.use(cors());
 app.use(express.json());
 
